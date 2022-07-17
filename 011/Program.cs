707059555b9e517c9d0f46f0054822504be2c013
Zhukov <42rus.ivan@gmail.com>
2022-07-18 02:59:02 +0300

    Add Task 011

---
+++ 011/Program.cs
@@ -0,0 +1,8 @@
+﻿// Task_011. Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+
+int a=123;
+int b=a/10;
+int c=a/10%10;
+//System.Console.WriteLine(b);
+System.Console.WriteLine(c);
+
