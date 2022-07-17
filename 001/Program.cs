0d80fcc2ba449e62912a42678210592ec14627b0
Zhukov <42rus.ivan@gmail.com>
2022-07-18 00:26:42 +0300

    Add Task 1

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Task_1. С клавиатуры вводится целое число. Вывести квадрат числа.
+int a;
+int b;
+string? s=Console.ReadLine();  // ввод данных
+a=Convert.ToInt32(s);
+
+b = a*a; // обработка данных 
+
+// Ниже указаны два варианта вывода результатов, лучше использовать второй.
+System.Console.WriteLine("{0}^2={1}",a,b); //строка форматирования, где 0 и 1 - считывают значения присвоенные для a,b и т.д
+System.Console.WriteLine($"{a}^2={b}"); //строка интерполяция при $
+
+
+
+/*
+string? s;
+s=Console.ReadLine();
+System.Console.WriteLine(s);
+*/
